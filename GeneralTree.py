27c5ff4e831685f209ff621d4e3650f9c715cb71
raynnz0907 <rayan310807@gmail.com>
2026-04-14 15:07:17 +0530

    Initializing parents and childeren

--- GeneralTree.py
+++ GeneralTree.py
@@ -12,10 +12,25 @@ def build_product_tree():
     root = TreeNode("Electronics")
 
     laptop = TreeNode("Laptop")
+    laptop.add_child("Mac")
+    laptop.add_child("HP")
+    laptop.add_child("ASUS")\
+    
+    cellphone = TreeNode("Cellphone")
+    cellphone.add_child("IPHONE")
+    cellphone.add_child("SAMSUNG")
+    cellphone.add_child("NOKIA")
+
+    tv = TreeNode("TV")
+    tv.add_child("SAMSUNG")
+    tv.add_child("LG")
 
     root.add_child(laptop)
+    root.add_child(tv)
+    root.add_child(cellphone)
 
 
 
 if __name__ == '__main__':
-    build_product_tree()
+    root = build_product_tree()
+    pass
