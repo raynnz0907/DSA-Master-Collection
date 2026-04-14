78093e3757969bca3563717381cad7ffa001f90c
raynnz0907 <rayan310807@gmail.com>
2026-04-14 15:16:35 +0530

    print func

--- GeneralTree.py
+++ GeneralTree.py
@@ -8,29 +8,38 @@ class TreeNode:
         child.parent = self
         self.children.append(child)
 
+    def print_tree(self):
+        print(self.data)
+        for child in self.children:
+            print(child.data)
+
 def build_product_tree():
     root = TreeNode("Electronics")
 
     laptop = TreeNode("Laptop")
-    laptop.add_child("Mac")
-    laptop.add_child("HP")
-    laptop.add_child("ASUS")\
-    
+    laptop.add_child(TreeNode("Mac"))
+    laptop.add_child(TreeNode("HP"))
+    laptop.add_child(TreeNode("ASUS"))
+        
     cellphone = TreeNode("Cellphone")
-    cellphone.add_child("IPHONE")
-    cellphone.add_child("SAMSUNG")
-    cellphone.add_child("NOKIA")
+    cellphone.add_child(TreeNode("IPHONE"))
+    cellphone.add_child(TreeNode("SAMSUNG"))
+    cellphone.add_child(TreeNode("NOKIA"))
+
 
     tv = TreeNode("TV")
-    tv.add_child("SAMSUNG")
-    tv.add_child("LG")
+    tv.add_child(TreeNode("SAMSUNG"))
+    tv.add_child(TreeNode("LG"))
 
     root.add_child(laptop)
     root.add_child(tv)
     root.add_child(cellphone)
 
+    return root 
+
 
 
 if __name__ == '__main__':
     root = build_product_tree()
+    root.print_tree()
     pass
