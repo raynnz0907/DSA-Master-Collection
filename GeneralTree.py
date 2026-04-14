194b5221e80877175bcd9a721fef9f9184b5db70
raynnz0907 <rayan310807@gmail.com>
2026-04-14 15:35:45 +0530

    get level function

--- GeneralTree.py
+++ GeneralTree.py
@@ -8,6 +8,14 @@ class TreeNode:
         child.parent = self
         self.children.append(child)
 
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1 
+            p = p.parent
+        return level
+
     def print_tree(self):
         print(self.data)
         if self.children:
