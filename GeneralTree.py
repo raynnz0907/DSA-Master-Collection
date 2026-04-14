19a8f90f70f1bc3225a6eb7e369b83533bd2dbd2
raynnz0907 <rayan310807@gmail.com>
2026-04-14 14:54:59 +0530

    creating a add funtion

--- GeneralTree.py
+++ GeneralTree.py
@@ -3,3 +3,7 @@ class TreeNode:
         self.data = data
         self.children = []
         self.parent = None
+    
+    def add_child(self,child):
+        child.parent = self
+        self.children.append(child)
