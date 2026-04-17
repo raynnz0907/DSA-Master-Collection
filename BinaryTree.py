2c594944b8d7d010dfa03d340e93ddbb1f3dc52a
raynnz0907 <rayan310807@gmail.com>
2026-04-17 11:09:50 +0530

    print in order transversal function

--- BinaryTree.py
+++ BinaryTree.py
@@ -7,17 +7,31 @@ class BinarySearchTreeNode:
     def add_data(self,data):
         if self.data == data:
             return
-        elif self.data < data:
-            if self.right:
-                self.right.add_data(data)
-            else:
-                self.right = BinarySearchTreeNode(data)
         
-        else:
+        elif self.data > data:
             if self.left:
                 self.left.add_data(data)
             else:
                 self.left = BinarySearchTreeNode(data)
-            
-
         
+        else:
+            if self.right:
+                self.right.add_data(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+    
+    def print_in_order_traversal(self):
+        elements = []
+
+        #Visiting the left tree
+        if self.left:
+            elements += self.left.print_in_order_traversal()
+        
+        #The root node
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.print_in_order_traversal()
+
+        return elements
+
