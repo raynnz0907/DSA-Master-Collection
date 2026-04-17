82ef4afa0cf33f29b87b7cf9f47f08c511f7b9ae
raynnz0907 <rayan310807@gmail.com>
2026-04-17 10:56:31 +0530

    Add data function

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,6 +1,23 @@
-class BinaryTree:
+class BinarySearchTreeNode:
     def __init__(self,data):
         self.data = data
         self.left = None
         self.right = None
+
+    def add_data(self,data):
+        if self.data == data:
+            return
+        elif self.data < data:
+            if self.right:
+                self.right.add_data(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+        
+        else:
+            if self.left:
+                self.left.add_data(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+            
+
         
